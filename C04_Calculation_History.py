3bdb201016130e42a45dbec1fb81aab920a10056
wescombef2 <66758208+wescombef2@users.noreply.github.com>
2021-07-08 13:41:09 +1200

    Update C04_Calculation_History.py

--- C04_Calculation_History.py
+++ C04_Calculation_History.py
@@ -1,9 +1,9 @@
 """
 Temperature Converter
-Component 02 - Converter GUI
-Version 1.0
+Component 04 - Calculation History
+Version 2.0
 Finn Wescombe
-28/06/21
+1/07/21
 """
 
 from tkinter import *
@@ -87,10 +87,10 @@ class Converter:
 
         # Help Button (HHBF - Row 0)
         self.btn_help = Button(self.frame_help_history,
-                                  text="Help",
-                                  font=("Arial", "14"),
-                                  padx=10, pady=10,
-                                  command=self.help)
+                               text="Help",
+                               font=("Arial", "14"),
+                               padx=10, pady=10,
+                               command=self.help)
         self.btn_help.grid(row=0, column=1)
 
         # History Button (HHBF - Row 0)
@@ -121,17 +121,23 @@ class Converter:
 
         # Add to a string the five most recent entries.
         if len(self.calculation_history) < 5:
-            for i in range(0,len(self.calculation_history)):
+            for i in range(0, len(self.calculation_history)):
                 # Calculation History format [[input, result], [input, result]].
                 # For most recent, use [-1]
-                entry = " >>> " + self.calculation_history[len(self.calculation_history) - i - 1][0] + " to " + self.calculation_history[len(self.calculation_history) - i - 1][1]
-                recent_history += entry + "\n"
+                entry = self.calculation_history[
+                    len(self.calculation_history) - i - 1][0] + " is " + \
+                        self.calculation_history[
+                            len(self.calculation_history) - i - 1][1]
+                recent_history += entry + "\n\n"
         else:
             for i in range(0, 5):
                 # Calculation History format [[input, result], [input, result]].
                 # For most recent, use [-1]
-                entry = " >>> " + self.calculation_history[len(self.calculation_history) - i - 1][0] + " to " + self.calculation_history[len(self.calculation_history) - i - 1][1]
-                recent_history += entry + "\n"
+                entry = self.calculation_history[
+                    len(self.calculation_history) - i - 1][0] + " is " + \
+                        self.calculation_history[
+                            len(self.calculation_history) - i - 1][1]
+                recent_history += entry + "\n\n"
 
         # Create class and configure history.
         get_history = History(self)
@@ -141,7 +147,7 @@ class Converter:
     def convert(self, centigrade):
 
         # Conversion Constant
-        CONVERSION = 9/5
+        CONVERSION = 9 / 5
 
         # Get Input and Determine Validity
         # Set input to string so that float '0.0' can be considered valid.
@@ -151,7 +157,8 @@ class Converter:
             input = float(self.entry_to_convert.get())
         except ValueError:
             # Configure Result Label Text to Display Error Message
-            self.lbl_convert_result.configure(text="This Input is Invalid. Please Enter a Number.", bg="red")
+            self.lbl_convert_result.configure(
+                text="This Input is Invalid. Please Enter a Number.", bg="red")
 
         if input or input == 0:
             # Determine Conversion Type
@@ -164,31 +171,43 @@ class Converter:
                     result = input * CONVERSION + 32
 
                     # Configure Result Label Text to Display Result Rounded to 1 Decimal Point with appropriate unit and green background.
-                    self.lbl_convert_result.configure(text="{:.1f} Fahrenheit".format(result), bg="green")
+                    self.lbl_convert_result.configure(
+                        text="{:.1f} Degrees Fahrenheit".format(result),
+                        bg="green")
 
                     # Save for Calculation History
-                    self.calculation_history.append(["{:.1f}".format(input) + "C", "{:.1f}".format(result) + "F"])
+                    self.calculation_history.append(
+                        ["{:.1f} Degrees Centigrade".format(input),
+                         "{:.1f} Degrees Fahrenheit".format(result)])
                     self.btn_history.configure(state=NORMAL)
 
                 else:
-                    self.lbl_convert_result.configure(text="This Temperature is Less than Absolute Zero (-273.13 Centigrade).", bg="red")
+                    self.lbl_convert_result.configure(
+                        text="This Temperature is Less than Absolute Zero (-273.13 Centigrade).",
+                        bg="red")
 
 
 
             else:
                 if input >= -459.76:
                     # Conversion to Centigrade from Fahrenheit
-                    result = (input - 32) * (1/CONVERSION)
+                    result = (input - 32) * (1 / CONVERSION)
 
                     # Configure Result Label Text to Display Result Rounded to 1 Decimal Point with appropriate unit.
-                    self.lbl_convert_result.configure(text="{:.1f} Centigrade".format(result), bg="green")
+                    self.lbl_convert_result.configure(
+                        text="{:.1f} Degrees Centigrade".format(result),
+                        bg="green")
 
                     # Save for Calculation History and enable button
-                    self.calculation_history.append(["{:.1f}".format(input) + "F", "{:.1f}".format(result) + "C"])
+                    self.calculation_history.append(
+                        ["{:.1f} Degrees Fahrenheit".format(input),
+                         "{:.1f} Degrees Centigrade".format(result)])
                     self.btn_history.configure(state=NORMAL)
 
                 else:
-                    self.lbl_convert_result.configure(text="This Temperature is Less than Absolute Zero (-459.76 Fahrenheit).", bg="red")
+                    self.lbl_convert_result.configure(
+                        text="This Temperature is Less than Absolute Zero (-459.76 Fahrenheit).",
+                        bg="red")
 
 
 # Help GUI Class
@@ -204,35 +223,37 @@ class Help:
         # Create Window
         self.window_help = Toplevel()
 
-        self.window_help.protocol('WM_DELETE_WINDOW', partial(self.dismiss_help, partner))
+        self.window_help.protocol('WM_DELETE_WINDOW',
+                                  partial(self.dismiss_help, partner))
 
         # Help Screen GUI
-        self.frame_help = Frame(self.window_help, width=300, height=200, bg=bg_colour)
+        self.frame_help = Frame(self.window_help, width=300, height=200,
+                                bg=bg_colour)
         self.frame_help.grid()
 
         # Help Heading (Row 0)
         self.lbl_heading_help = Label(self.frame_help,
-                                        text="Help/Instructions",
-                                        font=("Arial", "16", "bold"),
-                                        bg=bg_colour,
-                                        padx=10, pady=10)
+                                      text="Help/Instructions",
+                                      font=("Arial", "16", "bold"),
+                                      bg=bg_colour,
+                                      padx=10, pady=10)
         self.lbl_heading_help.grid(row=0)
 
         # Help Text (Label, Row 1)
         self.txt_help = Label(self.frame_help,
-                               text="",
-                               justify=LEFT,
-                               width=40,
-                               bg=bg_colour,
-                               wrap=250)
+                              text="",
+                              justify=LEFT,
+                              width=40,
+                              bg=bg_colour,
+                              wrap=250)
         self.txt_help.grid(row=1)
         # Help Button (Row 1)
         self.btn_dismiss = Button(self.frame_help,
-                                     text="Dismiss",
-                                     font=("Arial", "14"),
-                                     padx=10, pady=10,
-                                     width=10,
-                                     command=partial(self.dismiss_help, partner))
+                                  text="Dismiss",
+                                  font=("Arial", "14"),
+                                  padx=10, pady=10,
+                                  width=10,
+                                  command=partial(self.dismiss_help, partner))
         self.btn_dismiss.grid(row=2, pady=10)
 
     # Dismiss Help Function
@@ -241,6 +262,7 @@ class Help:
         partner.btn_help.configure(state=NORMAL)
         # Close Window
         self.window_help.destroy()
+
 
 # Help GUI Class
 class History:
@@ -255,37 +277,53 @@ class History:
         # Create Window
         self.window_history = Toplevel()
 
-        self.window_history.protocol('WM_DELETE_WINDOW', partial(self.dismiss_history, partner))
+        self.window_history.protocol('WM_DELETE_WINDOW',
+                                     partial(self.dismiss_history, partner))
 
         # History Screen GUI
-        self.frame_history = Frame(self.window_history, width=300, height=200, bg=bg_colour)
+        self.frame_history = Frame(self.window_history, width=300, height=200,
+                                   bg=bg_colour)
         self.frame_history.grid()
 
         # History Heading (Row 0)
         self.lbl_heading_history = Label(self.frame_history,
-                                        text="Calculation History",
-                                        font=("Arial", "16", "bold"),
-                                        bg=bg_colour,
-                                        padx=10, pady=10)
+                                         text="Calculation History",
+                                         font=("Arial", "16", "bold"),
+                                         bg=bg_colour,
+                                         padx=10, pady=5)
         self.lbl_heading_history.grid(row=0)
 
-        # Help Text (Row 1)
-        self.txt_history = Label(self.frame_history,
-                               text="",
-                               justify=LEFT,
-                               width=40,
-                               bg=bg_colour,
-                               wrap=250)
-        self.txt_history.grid(row=1)
+        # Instruction Label (Row 1)
+        self.lbl_instructions = Label(self.frame_history,
+                                      text="Here are your five most recent "
+                                           "calculations. You can export the "
+                                           "entire history to a .txt file using "
+                                           "the export button.",
+                                      font=("Arial", "10", "italic"),
+                                      wrap=250,
+                                      justify=CENTER,
+                                      bg=bg_colour,
+                                      padx=10, pady=10)
+        self.lbl_instructions.grid(row=1)
 
-        # Dismiss Button (Row 2)
+        # Recent History (Row 2)
+        self.txt_history = Label(self.frame_history,
+                                 text="",
+                                 font=("Arial", "10", "bold"),
+                                 justify=CENTER,
+                                 width=50,
+                                 bg=bg_colour)
+        self.txt_history.grid(row=2)
+
+        # Dismiss Button (Row 3)
         self.btn_dismiss = Button(self.frame_history,
-                                     text="Dismiss",
-                                     font=("Arial", "14"),
-                                     padx=10, pady=10,
-                                     width=10,
-                                     command=partial(self.dismiss_history, partner))
-        self.btn_dismiss.grid(row=2, pady=10)
+                                  text="Dismiss",
+                                  font=("Arial", "14"),
+                                  padx=10, pady=10,
+                                  width=10,
+                                  command=partial(self.dismiss_history,
+                                                  partner))
+        self.btn_dismiss.grid(row=3, pady=10)
 
     # Dismiss History Function
     def dismiss_history(self, partner):
